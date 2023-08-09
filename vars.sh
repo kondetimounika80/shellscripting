bc2febc3d9a3addf01fe9315bdeb10a2b40ea3e9
kondetimounika80 <128279253+kondetimounika80@users.noreply.github.com>
2023-08-09 14:31:20 +0530

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -9,3 +9,14 @@ echo "my name is $name and age is $age"
 name="mounika"
 
 echo "name is $name"
+
+#to store output of a cmd 
+HOSTNAME=$(hostname)
+echo "the name of the machine is $HOSTNAME"
+
+#constant variable
+
+readonly place="manthena"
+echo "my place is $place"
+
+place="hyd"
