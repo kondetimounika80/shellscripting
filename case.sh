ea7e9d43c718c06173f037810fe29d7e4dbb3e21
kondetimounika80 <128279253+kondetimounika80@users.noreply.github.com>
2023-08-09 15:11:20 +0530

    Create case.sh

---
+++ case.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "please select below option"
+echo "a for ptint date"
+echo "b for list the fles"
+echo "c for check the current loacation"
+
+read choice
+case $choice in 
+  a) 
+     echo "today date is :  "
+     date;;
+  b)ls;;
+  c)pwd;;
+  
